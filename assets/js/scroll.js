12ce541295d005a2bb63b86eb4f9bec8860af140
Caleb Tham <calebtham02@gmail.com>
2022-08-30 22:35:55 +0100

    Performance fix

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -1,15 +1,18 @@
 $(() => {
 
-    const mountain = $("#mountain")
+    const mountain = document.getElementById("mountain");
 
     $(window).scroll(() => {
         let scrollTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
         let brightness = 100 - 100 * scrollTop / window.innerHeight;
 
-        mountain.css({
-            top: Math.min(-Math.floor(scrollTop * 0.5), 0),
-            filter: `brightness(${brightness}%)`
-        });
+        // mountain.css({
+        //     top: Math.min(-Math.floor(scrollTop * 0.5), 0),
+        //     filter: `brightness(${brightness}%)`
+        // });
+
+        mountain.style.top = `${Math.min(-Math.floor(scrollTop * 0.5), 0)}px`;
+        mountain.style.filter = `brightness(${brightness}%)`;
     });
 
 });
