dc5334d5790457554a4e3987c65f1080444de553
Caleb Tham <calebtham02@gmail.com>
2022-08-30 22:46:42 +0100

    Parallax scroll on load

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -1,18 +1,14 @@
 $(() => {
-
     const mountain = document.getElementById("mountain");
+    
+    document.addEventListener("scroll", parallaxScroll);
 
-    $(window).scroll(() => {
+    parallaxScroll()
+    
+    function parallaxScroll() {
         let scrollTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
         let brightness = 100 - 100 * scrollTop / window.innerHeight;
-
-        // mountain.css({
-        //     top: Math.min(-Math.floor(scrollTop * 0.5), 0),
-        //     filter: `brightness(${brightness}%)`
-        // });
-
         mountain.style.top = `${Math.min(-Math.floor(scrollTop * 0.5), 0)}px`;
         mountain.style.filter = `brightness(${brightness}%)`;
-    });
-
+    }
 });
