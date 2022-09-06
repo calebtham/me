548dacc88e1dd964a465d066130052be1fd7be13
Caleb Tham <calebtham02@gmail.com>
2022-09-06 23:41:37 +0100

    Added content

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -1,31 +1,27 @@
-window.addEventListener("load", () => {
+window.addEventListener("DOMContentLoaded", () => {
     const mountain = document.getElementById("grindelwaldMountain");
     const water = document.getElementById("sunsetWater");
     const boat = document.getElementById("sunsetBoat");
     const arrow = document.getElementById("arrow");
     
-    document.addEventListener("scroll", parallaxScroll);
     lightSwitch.addEventListener("click", parallaxScroll);
+    
+    window.addEventListener("scroll", parallaxScroll);
 
     parallaxScroll()
     
     function parallaxScroll() {
-        let scrollTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
-        let brightness = 100 - 100 * scrollTop / window.innerHeight;
+        let translate = Math.max(window.scrollY, 0);
+        let brightness = 100 - 100 * translate / window.innerHeight;
        
         if (document.body.getAttribute("data-theme")) {
-            water.style.transform = `translateY(${Math.min(-Math.floor(scrollTop * 0.5), 0)}px)`;
-
-            boat.style.transform = `translate(${Math.max(Math.floor(scrollTop * 0.25), 0)}px, ${Math.min(-Math.floor(scrollTop * 0.5), 0)}px)`;
+            water.style.transform = `translateY(${-translate*0.5}px)`;   
+            boat.style.transform = `translate(${translate*0.25}px, ${-translate*0.5}px)`;
         } else {
-            mountain.style.transform = `translateY(${Math.min(-Math.floor(scrollTop * 0.5), 0)}px)`;
+            mountain.style.transform = `translateY(${-translate*0.5}px)`;
             mountain.style.filter = `brightness(${brightness}%)`;
         }
         
-        if (scrollTop === 0) {
-            arrow.style.opacity = "100%";
-        } else {
-            arrow.style.opacity = "0%";
-        }
+        arrow.style.opacity = (translate === 0) ? "100%" : "0%";
     }
 });
