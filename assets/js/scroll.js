015537595cafe1fffdc2aed6a1150969ae205360
Caleb Tham <calebtham02@gmail.com>
2022-09-18 00:57:54 +0100

    Scrolltrigger added

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -1,47 +1,57 @@
 /**
  * Parallax scroll effect
  */
+ gsap.to("#grindelwaldMountain", {
+    yPercent: -50,
+    ease: "none",
+    scrollTrigger: {
+      trigger: "#grindelwaldMountain",
+      start: "top", // the default values
+      end: "bottom", // the default values
+      scrub: true
+    }, 
+});
+
+gsap.to("#sunsetWater", {
+    yPercent: -50,
+    ease: "none",
+    scrollTrigger: {
+      trigger: "#sunsetWater",
+      start: "top", // the default values
+      end: "bottom", // the default values
+      scrub: true
+    }, 
+});
+
+gsap.to("#sunsetBoat", {
+    yPercent: -50,
+    xPercent: 25,
+    ease: "none",
+    scrollTrigger: {
+      trigger: "#sunsetBoat",
+      start: "top", // the default values
+      end: "bottom", // the default values
+      scrub: true
+    }, 
+});
+
+/**
+ * Change CSS on scroll
+ */
 window.addEventListener("DOMContentLoaded", () => {
     const mountain = document.getElementById("grindelwaldMountain");
-    const water = document.getElementById("sunsetWater");
-    const boat = document.getElementById("sunsetBoat");
     const arrow = document.getElementById("arrow");
     
-    if (window.innerWidth > 991) { // Desktop
-        window.addEventListener("scroll", parallaxScroll);
-        parallaxScroll();
-    } else { // Mobile
-        window.addEventListener("scroll", mobileScroll);
-    }
-    
-    /**
-     * Change position of elements when scrolling and show/hide arrow
-     */
-    function parallaxScroll() {
+    window.addEventListener("scroll", () => {
         let translate = Math.max(window.scrollY, 0);
         let brightness = 100 - 100 * translate / window.innerHeight;
        
         if (document.body.getAttribute("data-theme")) { // light mode
-            water.style.transform = `translateY(${-translate*0.5}px)`;   
-            boat.style.transform = `translate(${translate*0.25}px, ${-translate*0.5}px)`;
             arrow.style.color = (translate === 0) ? "black" : "#00000000";
             
         } else { // dark mode
-            mountain.style.transform = `translateY(${-translate*0.5}px)`;
             mountain.style.filter = `brightness(${brightness}%)`;
             arrow.style.color = (translate === 0) ? "white" : "#FFFFFF00";
         }
-    }
-
-    /**
-     * Show/hide arrow
-     */
-    function mobileScroll() {
-        let translate = Math.max(window.scrollY, 0);
-        if (document.body.getAttribute("data-theme")) { // light mode
-            arrow.style.color = (translate === 0) ? "black" : "#00000000";
-        } else { // dark mode
-            arrow.style.color = (translate === 0) ? "white" : "#FFFFFF00";
-        }
-    }
+    }); 
 });
